79f746e40d0d9cbca1ecd1c98acdc7718a0eec14
YonayCabrera <yonaycl@gmail.com>
2018-05-13 22:30:28 +0100

    integrated vue

---
+++ frontend/src/router/index.js
@@ -0,0 +1,22 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import CreateRoom from '@/components/CreateRoom'
+import Room from '@/components/Room'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'CreateRoom',
+      component: CreateRoom
+    },
+    {
+      path: '/Room/:name',
+      name: 'Room',
+      component: Room
+    }
+  ],
+  mode: 'history'
+})
